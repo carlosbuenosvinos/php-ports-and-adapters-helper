e12a3910aec8e7c6c6290f9958a681cf8a6e30b7
Carlos Buenosvinos <hi@carlos.io>
2013-09-30 21:34:15 +0200

    Tests

---
+++ src/Tests/GenericUseCaseRequestTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace HexArch\Tests;
+
+use HexArch\GenericUseCaseRequest;
+
+/**
+ * Class GenericUseCaseRequestTest
+ * @package HexArch\Tests
+ * @author Carlos Buenosvinos (hi@©arlos.io)
+ */
+class GenericUseCaseRequestTest extends \PHPUnit_Framework_TestCase
+{
+    private $request;
+
+    protected function setUp()
+    {
+        $this->request = new GenericUseCaseRequest();
+    }
+
+
+}
